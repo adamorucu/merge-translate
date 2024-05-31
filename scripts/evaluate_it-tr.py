c935c4615cb79aa6061c55fc70a04e412cff955d
Franco Ruggeri <franco.ruggeri@ericsson.com>
2024-05-31 20:17:24 +0200

    chore: minor changes

--- scripts/evaluate_it-tr.py
+++ scripts/evaluate_it-tr.py
@@ -11,10 +11,12 @@ def main():
     translator = ItalianTranslator(model_id="swap-uniba/LLaMAntino-2-chat-7b-hf-UltraChat-ITA")
     evaluate(translator=translator, from_language="tr", to_language="it")
     evaluate(translator=translator, from_language="it", to_language="tr")
+    del translator
 
     translator = TurkishTranslator(model_id="malhajar/Llama-2-7b-chat-tr")
     evaluate(translator=translator, from_language="tr", to_language="it")
     evaluate(translator=translator, from_language="it", to_language="tr")
+    del translator
 
     # TODO: complete for merged model
     # translator = MergedTranslator(model_id="")
