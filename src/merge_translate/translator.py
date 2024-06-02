8101ddf29f5c21acabe1b2663a4c4ab468d51410
Franco Ruggeri <franco.ruggeri@ericsson.com>
2024-06-02 22:17:56 +0200

    feat: mistral-based translator

--- src/merge_translate/translator.py
+++ src/merge_translate/translator.py
@@ -1,5 +1,4 @@
 from abc import ABC, abstractmethod
-import re
 from pathlib import Path
 from tqdm import tqdm
 import transformers as hf_transformers
@@ -10,23 +9,9 @@ import evaluate as hf_evaluate
 class Translator(ABC):
     """Prompt-based translator."""
 
-    def __init__(self, model_id: str) -> None:
-        self.model_id = model_id
-        self.pipeline = hf_transformers.pipeline(
-            task="text-generation",
-            model=model_id,
-            max_new_tokens=64,
-            device_map="auto",
-        )
-
     @abstractmethod
-    def _prepare_prompt(self, text: str, from_language: str, to_language: str) -> str:
-        raise NotImplementedError
-
     def __call__(self, text: str, from_language: str, to_language: str) -> str:
-        prompt = self._prepare_prompt(text, from_language, to_language)
-        output = self.pipeline(prompt, return_full_text=False)
-        return output[0]["generated_text"]
+        raise NotImplementedError
 
     def evaluate(self, from_language: str, to_language: str, output_path: str | Path | None = None) -> dict[str, float]:
         lang1, lang2 = (from_language, to_language) if from_language < to_language else (to_language, from_language)
@@ -57,63 +42,38 @@ class Translator(ABC):
         return results
 
 
-class ItalianTranslator(Translator):
+class MistralTranslator(Translator):
     code_to_language = {
-        "it": "Italiano",
-        "tr": "Turco",
-        "en": "Inglese",
+        "it": "Italian",
+        "pl": "Polish",
+        "en": "English",
     }
 
     prompt_template = (
-        "[INST]"
-        "<<SYS>>\n"
-        "Sei un assistente disponibile, rispettoso e onesto. Rispondi sempre nel modo piu' utile possibile, pur "
-        "essendo sicuro. Le risposte non devono includere contenuti dannosi, non etici, razzisti, sessisti, "
-        "tossici, pericolosi o illegali. Assicurati che le tue risposte siano socialmente imparziali e positive. "
-        "Se una domanda non ha senso o non e' coerente con i fatti, spiegane il motivo invece di rispondere in "
-        "modo non corretto. Se non conosci la risposta a una domanda, non condividere informazioni false.\n"
-        "<</SYS>>\n\n"
-        "Traduci la seguente frase da {from_language} a {to_language}: '{text}'\n"
-        "Formato della risposta: <translated text>"
-        "[/INST]"
+        "Translate the following sentence from {from_language} to {to_language} without adding any contextual "
+        "information: '{text}'"
     )
 
-    def __init__(self) -> None:
-        super().__init__(model_id="swap-uniba/LLaMAntino-2-chat-7b-hf-UltraChat-ITA")
+    def __init__(self, model_id: str = "mistralai/Mistral-7B-Instruct-v0.1"):
+        super().__init__()
+        self.model_id = model_id
+        self.model = hf_transformers.AutoModelForCausalLM.from_pretrained(self.model_id)
+        self.tokenizer = hf_transformers.AutoTokenizer.from_pretrained(self.model_id)
 
-    def _prepare_prompt(self, text: str, from_language: str, to_language: str) -> str:
-        return self.prompt_template.format(
+    def __call__(self, text: str, from_language: str, to_language: str) -> str:
+        prompt = self.prompt_template.format(
             from_language=self.code_to_language[from_language],
             to_language=self.code_to_language[to_language],
             text=text,
         )
-
-    def _extract_translation(self, output: str) -> str:
-        match = re.search(r"'([^']+)'", output)  # expected format: 'translated text'
-        return match.group(1) if match else output.strip()  # fallback to return the stripped output
-
-
-class TurkishTranslator(Translator):
-    code_to_language_from = {
-        "tr": "Türkçe'den",
-        "it": "İtalyanca'dan",
-        "en": "İngilizce'den",
-    }
-
-    code_to_language_to = {
-        "tr": "Türkçe'ye",
-        "it": "İtalyanca'ya",
-        "en": "İngilizce'ye",
-    }
-
-    prompt_template = "<s>[INST] Bu cümleyi {from_language} {to_language} çevir: '{text}' [/INST]"
-
-    def __init__(self):
-        super().__init__(model_id="malhajar/Llama-2-7b-chat-tr")
-
-    def _prepare_prompt(self, text: str, from_language: str, to_language: str) -> str:
-        return self.prompt_template.format(
-            from_language=self.code_to_language_from[from_language],
-            to_language=self.code_to_language_to[to_language],
-            text=text,
-        )
+        messages = [{"role": "user", "content": prompt}]
+        input_ids = self.tokenizer.apply_chat_template(messages, return_tensors="pt")
+        generated_ids = self.model.generate(
+            input_ids,
+            max_new_tokens=100,
+            do_sample=False,
+            pad_token_id=self.tokenizer.eos_token_id,
+        )[0, input_ids.shape[1] :]
+        decoded = self.tokenizer.decode(generated_ids, skip_special_tokens=True)
+        translation = decoded.replace("'", "").replace('"', "")
+        return translation
