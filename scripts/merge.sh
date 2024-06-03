6cc87a3c136cf7039771d6ea26f93c14e10bcd51
Franco Ruggeri <franco.ruggeri@ericsson.com>
2024-06-03 02:22:08 +0200

    feat: merging script, config, class

---
+++ scripts/merge.sh
@@ -0,0 +1,1 @@
+mergekit-yaml config/mistral_ties.yml ./models/Mistral-7B-Merged-v0.1/
--- src/merge_translate/translator.py
+++ src/merge_translate/translator.py
@@ -1,6 +1,7 @@
 from abc import ABC, abstractmethod
 from pathlib import Path
 from tqdm import tqdm
+import torch as th
 import transformers as hf_transformers
 import datasets as hf_datasets
 import evaluate as hf_evaluate
@@ -10,11 +11,17 @@ class Translator(ABC):
     """Prompt-based translator."""
 
     @abstractmethod
-    def __call__(self, text: str, from_language: str, to_language: str) -> str:
+    def __call__(self, text: str, source_language: str, target_language: str) -> str:
         raise NotImplementedError
 
-    def evaluate(self, from_language: str, to_language: str, output_path: str | Path | None = None) -> dict[str, float]:
-        lang1, lang2 = (from_language, to_language) if from_language < to_language else (to_language, from_language)
+    def evaluate(
+        self, source_language: str, target_language: str, output_path: str | Path | None = None
+    ) -> dict[str, float]:
+        lang1, lang2 = (
+            (source_language, target_language)
+            if source_language < target_language
+            else (target_language, source_language)
+        )
         dataset = hf_datasets.load_dataset(
             "Helsinki-NLP/tatoeba",
             lang1=lang1,
@@ -23,15 +30,15 @@ class Translator(ABC):
             trust_remote_code=True,
         )["translation"]
 
-        dataset = dataset[:100]  # TODO: for draft, a subset is ok... later we will use the full dataset
+        dataset = dataset[:1]  # TODO: for draft, a subset is ok... later we will use the full dataset
 
         def data_generator():
             for translation in dataset:
-                yield translation[from_language], translation[to_language]
+                yield translation[source_language], translation[target_language]
 
         metrics = hf_evaluate.combine(["bleu", "sacrebleu", "meteor", "chrf"])
         for x, y_true in tqdm(data_generator(), total=len(dataset), desc="Translating"):
-            y_pred = self(text=x, from_language=from_language, to_language=to_language)
+            y_pred = self(text=x, source_language=source_language, target_language=target_language)
             metrics.add(prediction=y_pred, reference=y_true)
         results = metrics.compute()
 
@@ -43,31 +50,32 @@ class Translator(ABC):
 
 
 class MistralTranslator(Translator):
-    code_to_language = {
+    code_target_language = {
         "it": "Italian",
-        "pl": "Polish",
+        "sv": "Swedish",
         "en": "English",
     }
 
     prompt_template = (
-        "Translate the following sentence from {from_language} to {to_language} without adding any contextual "
+        "Translate the following sentence from {source_language} to {target_language} without adding any contextual "
         "information: '{text}'"
     )
 
-    def __init__(self, model_id: str = "mistralai/Mistral-7B-Instruct-v0.1"):
+    def __init__(self):
         super().__init__()
-        self.model_id = model_id
-        self.model = hf_transformers.AutoModelForCausalLM.from_pretrained(self.model_id)
+        self.model_id = "mistralai/Mistral-7B-Instruct-v0.1"
+        self.device = "cuda" if th.cuda.is_available() else "cpu"
+        self.model = hf_transformers.AutoModelForCausalLM.from_pretrained(self.model_id).to(self.device).eval()
         self.tokenizer = hf_transformers.AutoTokenizer.from_pretrained(self.model_id)
 
-    def __call__(self, text: str, from_language: str, to_language: str) -> str:
+    def __call__(self, text: str, source_language: str, target_language: str) -> str:
         prompt = self.prompt_template.format(
-            from_language=self.code_to_language[from_language],
-            to_language=self.code_to_language[to_language],
+            source_language=self.code_target_language[source_language],
+            target_language=self.code_target_language[target_language],
             text=text,
         )
         messages = [{"role": "user", "content": prompt}]
-        input_ids = self.tokenizer.apply_chat_template(messages, return_tensors="pt")
+        input_ids = self.tokenizer.apply_chat_template(messages, return_tensors="pt").to(self.device)
         generated_ids = self.model.generate(
             input_ids,
             max_new_tokens=100,
@@ -75,5 +83,19 @@ class MistralTranslator(Translator):
             pad_token_id=self.tokenizer.eos_token_id,
         )[0, input_ids.shape[1] :]
         decoded = self.tokenizer.decode(generated_ids, skip_special_tokens=True)
-        translation = decoded.replace("'", "").replace('"', "")
+        translation = decoded.strip("\"'")
         return translation
+
+
+class MistralMergedTranslator(MistralTranslator):
+    def __init__(self, model_id: str):
+        # Due to an issue in mergekit, the merged model doesn't have the correct chat template.
+        # We need to manually set the chat template by using the one from Mistral-7B-Instruct-v0.1.
+        super().__init__()  # this will load the Mistral-7B-Instruct-v0.1 model
+        chat_template = self.tokenizer.chat_template  # cache the chat template
+
+        # Now, we can load the correct model and tokenizer
+        self.model_id = model_id
+        self.model = hf_transformers.AutoModelForCausalLM.from_pretrained(self.model_id).to(self.device).eval()
+        self.tokenizer = hf_transformers.AutoTokenizer.from_pretrained(self.model_id)
+        self.tokenizer.chat_template = chat_template  # set the correct chat template
